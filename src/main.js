678e201ece7f944215a991ccd0715d1013551ba3
intaaudio <cs@inta-audio.com>
2024-03-04 17:28:01 +0000

    Update CSS and JS for mobile responsiveness and enhanced UI
    Modified the CSS output file, adding styles for improved mobile friendliness and grid-based layout, and updating CSS classes for color and padding. Also revised JavaScript in main.js file to account for menu display on smaller screens. Cleanly reorganized the HTML structure with semantically appropriate tags and added new CSS classes. Additionally, the package-lock.json and package.json files were updated to include the GSAP animation library.

--- src/main.js
+++ src/main.js
@@ -4,16 +4,23 @@ const menuItems = () => {
   const mobileMenuHamburger = document.querySelector(".mob-menu-hamburger");
   const navMenu = document.querySelector("#nav-menu");
   const navMenuItems = document.querySelectorAll("#nav-menu li");
-  const headerLogoHamburger = document.querySelector("#header-info");
+  const navContainer = document.querySelector("#header-info");
+  const logo = document.querySelector("#logo");
 
-  return { mobileMenuHamburger, navMenu, navMenuItems, headerLogoHamburger };
+  return {
+    mobileMenuHamburger,
+    navMenu,
+    navMenuItems,
+    navContainer,
+    logo,
+  };
 };
 
 const navbarScrollEffect = () => {
-  menuItems().headerLogoHamburger.style.fontSize = "2.25rem";
+  menuItems().navContainer.style.fontSize = "2.25rem";
   menuItems().mobileMenuHamburger.style.fontSize = "2.25rem";
 
-  gsap.to("#header-info", {
+  gsap.to(["#header-info"], {
     background: "rgb(0,0,0)",
     color: "hsla(52, 94%, 94%, 1)",
 
@@ -24,13 +31,13 @@ const navbarScrollEffect = () => {
       end: "bottom 100px",
       scrub: true,
       onLeave: () => {
-        menuItems().headerLogoHamburger.style.fontSize = "1.5rem";
+        menuItems().navContainer.style.fontSize = "1.5rem";
         menuItems().mobileMenuHamburger.style.fontSize = "1.5rem";
-        menuItems().headerLogoHamburger.style.transition = "500ms";
+        menuItems().navContainer.style.transition = "500ms";
         menuItems().mobileMenuHamburger.style.transition = "500ms";
       },
       onEnterBack: () => {
-        menuItems().headerLogoHamburger.style.fontSize = "2.25rem";
+        menuItems().navContainer.style.fontSize = "2.25rem";
         menuItems().mobileMenuHamburger.style.fontSize = "2.25rem";
       },
     },
@@ -60,31 +67,40 @@ menuItemHoverAnim();
 
 const openMobileMenu = () => {
   const menuTimeline = gsap.timeline();
+  const mobile = gsap.matchMedia();
 
-  menuTimeline.pause();
+  mobile.add("(max-width: 768px)", () => {
+    menuTimeline.pause();
 
-  menuTimeline.fromTo(
-    "#nav-menu",
-    { xPercent: 100, opacity: 0 },
-    { xPercent: 0, duration: 0.3, opacity: 1 },
-  );
+    menuTimeline.fromTo(
+      "#nav-menu",
+      { xPercent: 100, opacity: 0 },
+      { xPercent: 0, duration: 0.3, opacity: 1 },
+    );
 
-  menuTimeline.fromTo(
-    ".mob-menu-item",
-    { opacity: 0, xPercent: 20 },
-    { opacity: 1, xPercent: 0, stagger: 0.07 },
-  );
+    menuTimeline.fromTo(
+      ".mob-menu-item",
+      { opacity: 0, xPercent: 20 },
+      { opacity: 1, xPercent: 0, stagger: 0.07 },
+    );
 
-  menuItems().mobileMenuHamburger.addEventListener("click", () => {
-    menuItems().navMenu.classList.toggle("open");
+    menuItems().mobileMenuHamburger.addEventListener("click", () => {
+      menuItems().navMenu.classList.toggle("open");
 
-    if (menuItems().navMenu.classList.contains("open")) {
-      menuTimeline.timeScale(1);
-      menuTimeline.play();
-    } else {
-      menuTimeline.timeScale(3);
-      menuTimeline.reverse();
-    }
+      const { logo, mobileMenuHamburger, navMenuItems } = menuItems();
+
+      if (menuItems().navMenu.classList.contains("open")) {
+        menuTimeline.timeScale(1);
+        menuTimeline.play();
+        logo.classList.add("text-black");
+        mobileMenuHamburger.classList.add("text-black");
+      } else {
+        menuTimeline.timeScale(3);
+        menuTimeline.reverse();
+        logo.classList.remove("text-black");
+        mobileMenuHamburger.classList.remove("text-black");
+      }
+    });
   });
 };
 openMobileMenu();
